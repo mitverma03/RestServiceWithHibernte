9bce0ae521393f44d4dfc000a6a326033f50c604
Mitesh Varma <Mitesh.Varma@rishabh.com>
2019-08-07 15:12:57 +0530

    commit created project

---
+++ src/main/java/com/example/RestServiceWithHibernate/RestServiceWithHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestServiceWithHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceWithHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceWithHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestServiceWithHibernate/RestServiceWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestServiceWithHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceWithHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
